d1ad65f02813450caf58e7a8eb1cc7b3c79fb24f
derysdominguez <derysdominguez@gmail.com>
2023-03-18 23:05:10 -0600

    home page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import './assets/styles/css/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from './views/Login';
+import Home from './views/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Login />
-    </>
+    <Router>
+      <Routes>
+      {/* <Route
+          path="/"
+          element={isLogged ? <Home token={local_token} /> : <Login />}
+      /> */}
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Container, Row, Form, Button, FormGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const handleSubmit = () => {};
+
   return (
     <>
       <Container className="d-flex justify-content-center align-items-center vh-100">
         <Row className="h-50 bg-light align-items-center text-center w-50 px-5">
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <h1>Bienvenido a ForaneosApp</h1>
             <Form.Group className="my-5" controlId="formBasicEmail">
               <Form.Control type="email" placeholder="Usuario" />
@@ -16,14 +19,18 @@ function Login() {
               <Form.Control type="password" placeholder="Contraseña" />
             </Form.Group>
 
+            <Link to='/home'>
               <Button variant="primary" type="submit" className="w-75">
                 Iniciar Sesion
               </Button>
-              <p className="mt-2 mb-5">¿No tienes cuenta? <b>Consulta con soporte.</b></p>
+            </Link>
+            <p className="mt-2 mb-5">
+              ¿No tienes cuenta? <b>Consulta con soporte.</b>
+            </p>
 
-              <Form.Text className="text-muted">
-                ¿Olvidaste tu contraseña?
-              </Form.Text>
+            <Form.Text className="text-muted">
+              ¿Olvidaste tu contraseña?
+            </Form.Text>
           </Form>
         </Row>
       </Container>
