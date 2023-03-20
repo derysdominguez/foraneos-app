52f8a1412ef069c6e55e218f67e47396a178589c
Luis Salgado <luissalgado877878@gmail.com>
2023-03-19 20:16:06 -0600

    modified login

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Form, Button, FormGroup } from "react-bootstrap";
+import { Container, Row, Form, Button, FormGroup, Col} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Login() {
@@ -8,30 +8,32 @@ function Login() {
   return (
     <>
       <Container className="d-flex justify-content-center align-items-center vh-100">
-        <Row className="h-50 bg-light align-items-center text-center w-50 px-5">
-          <Form onSubmit={handleSubmit}>
-            <h1>Bienvenido a ForaneosApp</h1>
-            <Form.Group className="my-5" controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="Usuario" />
-            </Form.Group>
+        <Row className="h-control bg-light align-items-center text-center px-4 py-4">
+          <Col>
+            <Form onSubmit={handleSubmit}>
+              <h2>Bienvenido a Foraneos-App</h2>
+              <Form.Group className="mt-5 mb-3" controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Usuario" />
+              </Form.Group>
 
-            <Form.Group className="mb-5" controlId="formBasicPassword">
-              <Form.Control type="password" placeholder="Contraseña" />
-            </Form.Group>
+              <Form.Group className="mb-4" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Contraseña" />
+              </Form.Group>
 
-            <Link to='/home'>
-              <Button variant="primary" type="submit" className="w-75">
-                Iniciar Sesion
-              </Button>
-            </Link>
-            <p className="mt-2 mb-5">
-              ¿No tienes cuenta? <b>Consulta con soporte.</b>
-            </p>
+              <Link to='/home'>
+                <Button variant="primary" type="submit" className="w-100">
+                  Iniciar Sesion
+                </Button>
+              </Link>
+              <p className="mt-3 mb-5 fs-6">
+                ¿No tienes cuenta?  <b>Consulta con soporte.</b>
+              </p>
 
-            <Form.Text className="text-muted">
-              ¿Olvidaste tu contraseña?
-            </Form.Text>
-          </Form>
+              <Form.Text className="text-muted mb-5">
+                ¿Olvidaste tu contraseña?
+              </Form.Text>
+            </Form>
+          </Col>
         </Row>
       </Container>
     </>
