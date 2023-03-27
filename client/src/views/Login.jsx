f2e7f3a6474abc78daa4655e0a7515ff5d4f8704
Fallen <axlsntz.dev@gmail.com>
2023-03-27 16:01:18 -0600

    se agregro la validacion de usuario en el login

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,36 +1,64 @@
-import React from "react";
-import { Container, Row, Form, Button, FormGroup, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useState } from 'react'
+import { Container, Row, Form, Button, FormGroup, Col } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
-  const handleSubmit = () => {};
+  const [credentials, setCredentials] = useState({ username: '', password: '' })
+  const [error, setError] = useState(null)
+  
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(credentials.username !== 'admin'){
+      return setError('Usuario incorrecto')
+    }
+    if(credentials.password !== 'admin'){
+      return setError('Contrasena incorrecta')
+    }
+    navigate('/reportes')
+  }
 
   return (
     <>
-      <Container className="d-flex justify-content-center align-items-center vh-100">
-        <Row className="h-control bg-light align-items-center text-center px-4 py-4">
+      <Container className='d-flex justify-content-center align-items-center vh-100'>
+        <Row className='h-control bg-light align-items-center text-center px-4 py-4'>
           <Col>
-            <Form onSubmit={handleSubmit}>
-
+            <Form onSubmit={handleSubmit} method='POST'>
               <h2>Bienvenido a Foraneos-App</h2>
 
-              <Form.Group className="mt-5 mb-3" controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Usuario" />
+              <Form.Group className='mt-5 mb-3' controlId='formBasicEmail'>
+                <Form.Control
+                  onChange={handleChange}
+                  name='username'
+                  placeholder='Usuario'
+                  value={credentials.username}
+                />
               </Form.Group>
 
-              <Form.Group className="mb-4" controlId="formBasicPassword">
-                <Form.Control type="password" placeholder="Contraseña" />
+              <Form.Group className='mb-2' controlId='formBasicPassword'>
+                <Form.Control
+                  onChange={handleChange}
+                  name='password'
+                  type='password'
+                  placeholder='Contraseña'
+                  value={credentials.password}
+                />
               </Form.Group>
-              <Link to='/reportes'>
-                <Button variant="primary" type="submit" className="w-100">
-                  Iniciar Sesion
-                </Button>
-              </Link>
-              <p className="mt-3 mb-5 fs-6">
-                ¿No tienes cuenta?  <b>Consulta con soporte.</b>
+
+              <p className='text-danger'>{error}</p>
+
+              <Button type='submit' variant='primary' className='w-100'>
+                Iniciar Sesion
+              </Button>
+              <p className='mt-3 mb-5 fs-6'>
+                ¿No tienes cuenta? <b>Consulta con soporte.</b>
               </p>
 
-              <Form.Text className="text-muted mb-5">
+              <Form.Text className='text-muted mb-5'>
                 ¿Olvidaste tu contraseña?
               </Form.Text>
             </Form>
@@ -38,7 +66,7 @@ function Login() {
         </Row>
       </Container>
     </>
-  );
+  )
 }
 
-export default Login;
+export default Login
