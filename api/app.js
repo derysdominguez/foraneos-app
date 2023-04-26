ed3fa3a5e02b06279c73b83cc18881408cae94a9
Fallen <axlsntz.dev@gmail.com>
2023-04-26 03:12:03 -0600

    upd

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const indexRoutes = require("./routes/index.routes.js");
-
+const cors = require('cors');
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Aquí especifica el dominio permitido
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -11,6 +11,8 @@ app.use((req, res, next) => {
   
 
 app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
 app.use(indexRoutes);
 
 module.exports = app;
